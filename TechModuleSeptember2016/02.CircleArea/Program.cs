baabe3d759249b79b37d5eb16155ea0330ec31c4
GordonFr33man <slipknoterbg@gmail.com>
2016-09-25 11:31:02 +0300

    HomeworkUpdate25.9.2016

---
+++ TechModuleSeptember2016/02.CircleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double r = double.Parse(Console.ReadLine());
+            double area = r * r * Math.PI;
+            Console.WriteLine($"{area:f12}");
+        }
+    }
+}
---
+++ TechModuleSeptember2016/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                decimal digit = decimal.Parse(Console.ReadLine());
+
+                sum += digit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
