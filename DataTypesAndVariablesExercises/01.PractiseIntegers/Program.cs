44451c92ad069041593c2f5253bdf42c221910b2
GordonFr33man <slipknoterbg@gmail.com>
2016-09-25 20:17:15 +0300

    TechModuleExercise

---
+++ DataTypesAndVariablesExercises/01.PractiseIntegers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.PractiseIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            sbyte numberOne = -100;
+            byte numberTwo = 128;
+            short numberThree = -3540;
+            ushort numberFour = 64876;
+            uint numberFive = 2147483648;
+            int numberSix = -1141583228;
+            long numberSeven = -1223372036854775808;
+            Console.WriteLine(numberOne);
+            Console.WriteLine(numberTwo);
+            Console.WriteLine(numberThree);
+            Console.WriteLine(numberFour);
+            Console.WriteLine(numberFive);
+            Console.WriteLine(numberSix);
+            Console.WriteLine(numberSeven);
+           
+
+
+
+
+
+
+        }
+    }
+}
